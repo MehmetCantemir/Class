20d011f9352945fcc1f3fc5463faf82e23e38cfa
mehmet cantemir <mehmetcantemir81@gmail.com>
2022-12-09 15:13:04 +0300

    C# Class

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calisan calisan1= new Calisan();
+            calisan1.Ad = "Mehmet";
+            calisan1.SoyAd = "Cantemir";
+            calisan1.No = 21321414;
+            calisan1.Departman = "BT";
+            calisan1.CalisanBilgileri();
+        }
+             
+    }
+    class Calisan
+    {
+        public string Ad;
+
+        public string SoyAd;
+
+        public int No;
+
+        public string Departman;
+
+        public void CalisanBilgileri()
+        {
+            Console.WriteLine("Çalışan Adı :{0} ",Ad);
+            Console.WriteLine("Çalışan SoyAdı :{0} ", SoyAd);
+            Console.WriteLine("Çalışan No :{0} ", No);
+            Console.WriteLine("Çalışan Departman :{0} ", Departman);
+
+        }
+
+    }
+}
